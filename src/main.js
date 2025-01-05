7f7b64d282174dfd370c6a4357b6bd92965977d0
Archie Wang <cansoking@hotmail.com>
2025-01-05 20:32:22 +0800

    home-图片懒加载实现

--- src/main.js
+++ src/main.js
@@ -6,9 +6,24 @@ import router from "./router";
 
 import "@/styles/common.scss";
 
+import { useIntersectionObserver } from "@vueuse/core";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+// 定义全局指令
+app.directive("img-lazy", {
+  mounted(el, binding) {
+    // el: 指令绑定的那个元素
+    // binding: binding.value 是指令的值
+    useIntersectionObserver(el, ([{ isIntersecting }]) => {
+      if (isIntersecting) {
+        el.src = binding.value;
+      }
+    });
+  },
+});
