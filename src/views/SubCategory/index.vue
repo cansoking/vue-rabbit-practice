47b9eaca7e375a3142d4ee294b9754109af76078
Archie Wang <cansoking@hotmail.com>
2025-01-09 21:24:08 +0800

    二级分类-无限滚动实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -28,13 +28,27 @@ const getGoodsList = async () => {
   const res = await getSubCategoryAPI(reqParams.value);
   goodsList.value = res.result.items;
 };
+onMounted(() => {
+  getGoodsList();
+});
+
+// Tab切换
 const tabChange = () => {
   reqParams.value.page = 1;
   getGoodsList();
 };
-onMounted(() => {
-  getGoodsList();
-});
+
+// 无线滚动加载
+const disabled = ref(false);
+const load = async () => {
+  reqParams.value.page++;
+  const res = await getSubCategoryAPI(reqParams.value);
+  goodsList.value = [...goodsList.value, ...res.result.items];
+  // 加载完毕 停止监听
+  if (res.result.items.length === 0) {
+    disabled.value = true;
+  }
+};
 </script>
 
 <template>
@@ -55,7 +69,11 @@ onMounted(() => {
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div
+        class="body"
+        v-infinite-scroll="load"
+        :infinite-scroll-disabled="disabled"
+      >
         <!-- 商品列表-->
         <GoodsItem v-for="good in goodsList" :good="good" :key="good.id" />
       </div>
