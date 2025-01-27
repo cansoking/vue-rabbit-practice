1074b541b8ada2b2ceb31867f6755104792e2643
ArchieWang <cansoking@hotmail.com>
2025-01-27 13:36:50 +0800

    Cart-本地-列表购物车-本地合并购物车

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,6 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { loginAPI } from "@/apis/user";
 import { useCartStore } from "./cart";
+import { mergeCartAPI } from "@/apis/cart";
 
 export const useUserStore = defineStore(
   "user",
@@ -13,6 +14,14 @@ export const useUserStore = defineStore(
       const res = await loginAPI({ account, password });
       userInfo.value = res.result;
 
+      // 合并购物车
+      await mergeCartAPI(
+        cartStore.cartList.map((item) => ({
+          skuId: item.skuId,
+          count: item.count,
+          selected: item.selected,
+        }))
+      );
       // 更新购物车数据
       cartStore.updateNewCartList();
     };
